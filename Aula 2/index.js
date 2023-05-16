ff7e8f070c757bd8078b804523744eac7d574b96
Graziele Reis <103457770+grareis@users.noreply.github.com>
2023-05-16 16:25:32 -0300

    Update index.js

--- Aula 2/index.js
+++ Aula 2/index.js
@@ -1,3 +1,37 @@
+//Código alterado para inserção do botão de excluir "x" da lista:
+var entrada = document.querySelector('#entrada');
+var botao = document.querySelector('#botao');
+var lista = document.getElementsByTagName('ul')[0];
+
+botao.onclick = function(){
+    let msg = entrada.value;
+    let listItem = document.createElement('li');
+    listItem.className = 'list-group-item';
+    listItem.textContent = msg;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.className = 'btn btn-danger btn-sm float-right';
+    deleteButton.textContent = 'x';
+    
+    deleteButton.onclick = function() {
+        listItem.remove();
+    };
+    
+    listItem.appendChild(deleteButton);
+    lista.appendChild(listItem);
+    
+    entrada.value = '';
+};
+
+entrada.addEventListener('keyup', function(event){
+    event.preventDefault();
+    if(event.keyCode == 13){
+      botao.click();
+    }    
+});
+
+
+/*CÓDIGO CRIADO EM AULA:
 var entrada = document.querySelector('#entrada');
 //console.log(entrada);
 var botao = document.querySelector('#botao');
@@ -6,7 +40,7 @@ var lista = document.getElementsByTagName('ul')[0];
 /*botao.onclick = function(){
     var msg = entrada.value;
     console.log(`botao clicado: ${msg}`);
-}*/
+
 
 botao.onclick = function(){
     let msg = entrada.value;
@@ -19,11 +53,11 @@ entrada.addEventListener('keyup', function(event){
     /*let aux = event.target.value;
     console.log(aux);
     console.log('key:'+ event.key );
-    console.log('keyCode:' + event.keyCode);*/
+    console.log('keyCode:' + event.keyCode);
     event.preventDefault();
     if(event.keyCode == 13){
       //console.log(`Tarefa: ${event.target.value}`);  
       botao.click();
     }    
     
-});
+});*/
